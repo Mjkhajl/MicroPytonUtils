bea16d1d549bd4ad5ade2f7f0c224284e81a9249
Mjkhajl <luismiguel.arteaga@gmail.com>
2016-11-03 10:54:24 -0600

    updates to the server, host address lookup added and close operation

--- MicroPyWebServer/webserver/server.py
+++ MicroPyWebServer/webserver/server.py
@@ -1,34 +1,41 @@
 import socket as socket
+from network import WLAN
 
-def start( host, port):
+class Server:
 
-    CONTENT = """\
-HTTP/1.0 200 OK
+    CONTENT = """\\\nHTTP/1.0 200 OK\n\nHello #{} from MicroPython!\n\n"""
 
-Hello #{} from MicroPython!
+    def __init__( self, port ):
+        self.port = port
+        self.host = WLAN().ifconfig()[0]
 
-"""
+    def start( self ):
 
-    ai = socket.getaddrinfo( host, port)
-    addr = ai[0][4]
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(addr)
-    s.listen(5)
-    counter=0
+        print( "starting...", self.host, ":", self.port )
+        self.s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+        print( socket.getaddrinfo( self.host, self.port ) )
+        self.s.bind( socket.getaddrinfo( self.host, self.port )[0][4] )
+        self.s.listen( 5 )
+        counter = 0
 
-    while True:
-        res = s.accept()
-        client_s = res[0]
-        client_addr = res[1]
-        print("Client address:", client_addr)
-        print("Client socket:", client_s)
-        print("Request:")
-        req = client_s.recv(4096)
-        print(req)
-        client_s.send(bytes(CONTENT.format(counter), "ascii"))
-        client_s.close()
-        parts = req.decode('ascii').split(' ')
-        if parts[1] == '/exit':
-            break
-        counter += 1
-    
+        while True:
+            res = self.s.accept()
+            client_s = res[0]
+            client_addr = res[1]
+            print( "Client address:", client_addr )
+            print( "Client socket:", client_s )
+            print( "Request:" )
+            req = client_s.recv( 4096 )
+            print( req )
+            client_s.send( bytes( Server.CONTENT.format( counter ), "ascii" ) )
+            client_s.close()
+            parts = req.decode( 'ascii' ).split( ' ' )
+            if parts[1] == '/exit':
+                break
+            counter += 1
+
+    def stop( self ):
+
+        print( "close socket... " )
+        self.s.close();
+
